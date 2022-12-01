eff3bfa579f8ffb8489719a6a8a6308c2f84d057
kg-cornell <112091075+kg-cornell@users.noreply.github.com>
2022-12-01 13:40:26 -0500

    Update IDD-Final-v1.py

--- IDD-Final-v1.py
+++ IDD-Final-v1.py
@@ -1,11 +1,11 @@
 import RPi.GPIO as GPIO          
 from time import sleep
 
-in1 = 24 #change me to actual GPIO physical pin number
-in2 = 23 #change me to actual GPIO physical pin number
+in1 = 5 #change me to actual GPIO physical pin number
+in2 = 6 #change me to actual GPIO physical pin number
 
-in3 = 10 #change me to actual GPIO physical pin number
-in4 = 11 #change me to actual GPIO physical pin number
+in3 = 20 #change me to actual GPIO physical pin number
+in4 = 21 #change me to actual GPIO physical pin number
 
 #Set the intended pins as inputs/outputs for the GPIO
 GPIO.setmode(GPIO.BCM)
@@ -22,40 +22,82 @@ GPIO.output(in2,GPIO.LOW)
 GPIO.output(in3,GPIO.LOW)
 GPIO.output(in4,GPIO.LOW)
 
+print("device is prepared to run")
+sleep(2)
+
 #This loop goes on forever
 while(1):
-    #Rotate the 1 motor in the direction of "IN-1"
+    print("in loop")
+    
+    print("move forward")
+    sleep(2)
+    #Move forward
     GPIO.output(in1,GPIO.HIGH)
-
-    #Make sure the direction "IN-2" is off
     GPIO.output(in2,GPIO.LOW)
-    sleep(10) #Let the motor run for x amount of seconds
-
-    GPIO.output(in1,GPIO.LOW)
-    #Make sure the direction "IN-2" is off
-    GPIO.output(in2,GPIO.LOW)
-    sleep(3) #Let the motor run for x amount of seconds
-
-    #Rotate the 1 motor in the direction of "IN-2"
-    GPIO.output(in2,GPIO.HIGH)
-    #Make sure the direction "IN-1" is off
-    GPIO.output(in1,GPIO.LOW)
-    sleep(10) #Let the motor run for x amount of seconds
-   
-    #Rotate the 1 motor in the direction of "IN-3"
-    GPIO.output(in3,GPIO.HIGH)
-
-    #Make sure the direction "IN-4" is off
-    GPIO.output(in4,GPIO.LOW)
-    sleep(10) #Let the motor run for x amount of seconds
-
     GPIO.output(in3,GPIO.LOW)
-    #Make sure the direction "IN-4" is off
-    GPIO.output(in4,GPIO.LOW)
-    sleep(3) #Let the motor run for x amount of seconds
-
-    #Rotate the 1 motor in the direction of "IN-4"
     GPIO.output(in4,GPIO.HIGH)
-    #Make sure the direction "IN-3" is off
+    sleep(10)
+
+    print("stop")
+    sleep(2)
+    #Stop
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
     GPIO.output(in3,GPIO.LOW)
-    sleep(10) #Let the motor run for x amount of seconds
+    GPIO.output(in4,GPIO.LOW)
+    sleep(5)
+    
+    print("turn left")
+    sleep(2)
+    #Turn Left
+    GPIO.output(in1,GPIO.HIGH)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
+    sleep(10)
+
+    print("stop")
+    sleep(2)
+    #Stop
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    sleep(5)
+    
+    print("move backward")
+    sleep(2)
+    #Move backward
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.HIGH)
+    GPIO.output(in4,GPIO.LOW)
+    sleep(10)
+
+    print("stop")
+    sleep(2)
+    #Stop
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    sleep(5)
+    
+    print("turn right")
+    sleep(2)
+    #Turn Right
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.HIGH)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.HIGH)
+    sleep(10)
+    
+    print("stop")
+    sleep(2)
+    #Stop
+    GPIO.output(in1,GPIO.LOW)
+    GPIO.output(in2,GPIO.LOW)
+    GPIO.output(in3,GPIO.LOW)
+    GPIO.output(in4,GPIO.LOW)
+    sleep(5)
+    break
